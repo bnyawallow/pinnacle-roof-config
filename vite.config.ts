ddd42914beab79bfc6e11c8b2e265219fdad856f
bnyawallow <bnyawallow@gmail.com>
2025-10-16 11:58:39 +0300

    using browser router

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/pinnacle-roof-config" : "/",
+  base: mode === "production" ? "/pinnacle-roof-config/" : "/",
   server: {
     host: "::",
     port: 8080,
