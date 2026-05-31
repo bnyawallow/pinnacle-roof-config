6cbc364ba2bab9142e9178e0325120ed423b739c
bnyawallow <bnyawallow@gmail.com>
2026-05-31 17:23:59 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,33 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig, loadEnv } from 'vite';
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react(), tailwindcss()],
+    define: {
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
-  },
-}));
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+    server: {
+      host: true,
+      allowedHosts: ['.creativefringe.digital'],
+      port: 3000,
+      strictPort: true,
+      hmr: env.DISABLE_HMR !== 'true',
+    },
+    preview: {
+      host: true,
+      allowedHosts: ['.creativefringe.digital'],
+      port: 3000,
+      strictPort: true,
+    },
+  };
+});
